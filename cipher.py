8c9e2769f5defc35fb121e2cd4a1042feac5aa69
freshskates <business.robertcacho@gmail.com>
2021-02-01 19:31:04 -0800

    added Affine Encryption

--- cipher.py
+++ cipher.py
@@ -17,19 +17,23 @@ class Encryption:
         return "".join(Encryption.getChar((Encryption.getIndex(x) + key * mode) % 26) if x.isalpha() else x
                        for x in phrase.lower())
 
+    @staticmethod
+    def affine(phrase: str, a, b) -> str:
+        return "".join(Encryption.getChar((a * Encryption.getIndex(x) + b) % 26)if x.isalpha() else x
+                       for x in phrase.lower())
+
 
 if __name__ == "__main__":
 
     plain = input("Enter phrase to cipher: ")
-    key = int(input("Enter key: "))
-    print("Original: " + plain)
-    print("Cipher: " + Encryption.cipher(plain))
-    print("Decipher: " + Encryption.cipher(plain, -1))
-
-
-# getIndex = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7, "i": 8, "j": 9, "k": 10, "l": 11, "m": 12,
-#           "n": 13, "o": 14, "p": 15, "q": 16, "r": 17, "s": 18, "t": 19, "u": 20, "v": 21, "w": 22, "x": 23, "y": 24, "z": 25}
-
-# getChar = {value: key for key, value in getIndex.items()}
-
-# print("".join([getChar[(getIndex[x] + key) % 26] for x in plain.lower()]))
+    print("Press 0 for Caesar cipher \npress 1 for Affine Cipher \npress 2 for Brute Force \n")
+    option = int(input("Your option : "))
+    if(option == 0):
+        key = int(input("Enter key: "))
+        print("Original: " + plain)
+        print("Cipher: " + Encryption.cipher(plain))
+        print("Decipher: " + Encryption.cipher(plain, -1))
+    elif(option == 1):
+        a = int(input("enter a: "))
+        b = int(input("enter b: "))
+        print("Cipher: " + Encryption.affine(plain, a, b))
